ccc8b0beb0d6d0f25269e5800c9d3d7641d94e2f
arian the dude <arianthedude@arians-MacBook-Air.local>
2025-12-24 12:26:56 +0330

    added tailwind

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,8 +1,9 @@
 <script setup>
-
-
+import SideNav from './SideNav.vue'
 </script>
 
 <template>
-
+  <div>
+    <SideNav />
+  </div>
 </template>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside>
+    <h2 class="py-5">AvoBurger</h2>
+    <nav>
+      <ul>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Orders</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Drivers</a></li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<script setup></script>

770b7be8aa2f25b86f81f1c0fa18399f8a22ae8b
arian the dude <arianthedude@arians-MacBook-Air.local>
2025-12-26 12:53:47 +0330

    added metadata

--- src/router/index.js
+++ src/router/index.js
@@ -8,28 +8,48 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Home ',
+        description: 'Welcome to my awesome app',
+      },
     },
     {
       path: '/products',
       name: 'products',
       component: () => import('../views/ProductsView.vue'),
+      meta: {
+        title: 'Products ',
+        description: 'Browse our products',
+      },
     },
-     {
+    {
       path: '/restaurants',
       name: 'restaurants',
       component: () => import('../views/RestaurantsView.vue'),
+      meta: { title: 'Restaurants ' },
     },
     {
       path: '/orders',
       name: 'orders',
       component: () => import('../views/OrdersView.vue'),
+      meta: { title: 'Orders ' },
     },
     {
       path: '/drivers',
       name: 'drivers',
       component: () => import('../views/DriversView.vue'),
-    }
+      meta: { title: 'Drivers ' },
+    },
   ],
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || 'My App'
+
+  const descriptionMeta = document.querySelector('meta[name="description"]')
+  if (descriptionMeta) {
+    descriptionMeta.setAttribute('content', to.meta.description || 'Default description')
+  }
 })
 
 export default router
